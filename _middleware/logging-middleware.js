02c60d8045530c506ae4e6c2138448b4ced38827
alex-dev-888 <alex.dev.88888@gmail.com>
2024-10-24 14:31:54 +0700

    Fix bug LOGGING_ENABLED=0
    Fix bug LOGGING_ENABLED=0

--- _middleware/logging-middleware.js
+++ _middleware/logging-middleware.js
@@ -1,9 +1,11 @@
 const { infoLogger, errorLogger } = require('../_helpers/logger')
 
 module.exports = (req, res, next) => {
-  infoLogger.info(
-    `${req.requestId} - ${req.method} ${req.url} ${JSON.stringify(req.body)}`
-  )
+  if (infoLogger) {
+    infoLogger.info(
+      `${req.requestId} - ${req.method} ${req.url} ${JSON.stringify(req.body)}`
+    )
+  }
 
   const startTime = Date.now()
   let oldSend = res.send
@@ -15,13 +17,15 @@ module.exports = (req, res, next) => {
 
   res.on('finish', () => {
     const duration = Date.now() - startTime
-    infoLogger.info(
-      `${req.requestId} - ${res.statusCode} ${res.statusMessage}; ${
-        res.get('Content-Length') || 0
-      }b sent; Headers: ${JSON.stringify(
-        res.getHeaders()
-      )}; Duration: ${duration}ms; Body: ${JSON.stringify(res.body)}`
-    )
+    if (infoLogger) {
+      infoLogger.info(
+        `${req.requestId} - ${res.statusCode} ${res.statusMessage}; ${
+          res.get('Content-Length') || 0
+        }b sent; Headers: ${JSON.stringify(
+          res.getHeaders()
+        )}; Duration: ${duration}ms; Body: ${JSON.stringify(res.body)}`
+      )
+    }
   })
   next()
 }
